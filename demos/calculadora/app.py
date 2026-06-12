2f52c73502203ec995e21139ed29e93c078e59ad
poovx0306 <angel.poovx@gmail.com>
2026-06-12 14:20:24 +0000

    Updated Calculadora

--- demos/calculadora/app.py
+++ demos/calculadora/app.py
@@ -14,27 +14,39 @@ class Index:
 
 class Calculadora:
     def GET(self):
-        numero1=''
-        numero2=''
-        resultado=''
-        return render.calculadora(numero1, numero2, resultado,operacion)
+        numero1 = ''
+        numero2 = ''
+        resultado = ''
+        return render.calculadora(numero1, numero2, resultado)
     
     def POST(self):
         formulario = web.input()
         numero1 = float(formulario['numero1'])
         numero2 = float(formulario['numero2'])
-        operacion= formulario['operacion']
+        operacion = formulario['operacion']
+        resultado=''
 
-        if operacion == 'suma':
-             resultado = numero1 + numero2
-
-        if operacion == 'resta':
+        if operacion == 'sumar':
+            resultado = numero1 + numero2
+        if operacion == 'restar':
             resultado = numero1 - numero2
+        if operacion == 'multiplicar':
+            resultado = numero1*numero2
+        if operacion == 'dividir':
+            if numero2!=0:
+                resultado = numero1/numero2
+        if operacion == 'raiz':
+            resultado = numero1 ** (1/numero2)
+        if operacion=='potencia':
+            resultado = numero1 ** numero2
+        if operacion == 'modulo':
+            resultado = numero1 % numero2
+        if operacion == 'limpiar':
+            numero1=''
+            numero2=''
+            resultado=''
 
+        return render.calculadora(numero1, numero2, resultado)
 
-        return render.calculadora(numero1, numero2, resultado,operacion)
-        
-        
-        
 if __name__ == "__main__":
     app.run()
